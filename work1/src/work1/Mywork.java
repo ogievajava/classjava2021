bb8df91c337fefb7b338f451257469e2a2e971d6
admin CIFO <admin CIFO@DESKTOP-340U509>
2021-04-21 13:38:54 +0200

    work donwe

--- work1/src/work1/Mywork.java
+++ work1/src/work1/Mywork.java
@@ -1,5 +1,7 @@
 package work1;
 
+import java.util.Scanner;
+
 public class Mywork {
 
 	public static void main(String[] args) {
@@ -7,5 +9,14 @@ public class Mywork {
    System.out.println("How are you?");
    System.out.println("Answer when you can ");
 	}
- 
+	Scanner reader = new Scanner (System.in);
+	
+	{
+		Scanner name; 
+		 name = new Scanner (System.in); 
+		 if (name.equals("James")) {
+		 }
+		System.out.println(name + "Answer when you can ");
+	}
+	
 }
