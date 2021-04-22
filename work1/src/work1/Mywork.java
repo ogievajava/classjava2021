ea32a04f0d27420e298f083223bae3719fa02c00
admin CIFO <admin CIFO@DESKTOP-340U509>
2021-04-22 13:48:25 +0200

    various methodos y 2 ambit

--- work1/src/work1/Mywork.java
+++ work1/src/work1/Mywork.java
@@ -10,6 +10,9 @@ public class Mywork {
 			int totalDiscount = 0;
 			int amountTopay = 0;
 			// scope #6
+		while(true) {
+			
+	
 			Scanner reader = new Scanner(System.in);
 			System.out.println("Tell me your client name:");
 			String clientName = reader.nextLine();
@@ -24,38 +27,42 @@ public class Mywork {
 			discount = 25;
 			totalDiscount = clientBuy * 25/100;
 			amountTopay = clientBuy - totalDiscount;
+			printTicket(clientName, clientType, clientBuy, discount, totalDiscount, amountTopay);
 			
 			}else if (clientType.equals("Regular")) {
 			discount = 15;
+			totalDiscount = clientBuy * 15/100;
+			amountTopay = clientBuy - totalDiscount;
+			printTicket(clientName, clientType, clientBuy, discount, totalDiscount, amountTopay);
 			
 			}else if (clientType.equals("New")) {
 			discount = 0;
-				
+			totalDiscount = clientBuy * 0/100;
+			amountTopay = clientBuy - totalDiscount;
+			printTicket(clientName, clientType, clientBuy, discount, totalDiscount, amountTopay);
+			
 			}else {
 				System.out.println("Hello " + clientName + ", YOU ARE NOT A REGISTERED MEMBER");
 			}
 			
 			
-			System.out.println("\nclient:");
-			System.out.println("-------------------------\n");
-			System.out.println("client Name: " + clientName);
-			System.out.println("client Type: " + clientType);
-			System.out.println("client Buy: " + clientBuy);
-			System.out.println("Your discout is: " + discount);
-			System.out.println("Your total discout is: " + totalDiscount);
-			System.out.println("You have to pay : " + amountTopay + "\n");
 			
 				
 			// I am closing the while so also the while scope
 		}
 
-	
-
-	public static void methodToTestScope() {
-		// scope number #5
-		// here i don't see variables from other scopes
-		// clientName <<<< i don't see this variable from scope #2
-		// reader <<<< i don't see this variable from scope #2
-
 	}
+
+	public static void printTicket(String clientName, String clientType, int clientBuy, int discount, int totalDiscount, int amountTopay ) {
+		
+		System.out.println("\nclient:");
+		System.out.println("-------------------------\n");
+		System.out.println("client Name: " + clientName);
+		System.out.println("client Type: " + clientType);
+		System.out.println("client Buy: " + clientBuy);
+		System.out.println("Your discout is: " + discount);
+		System.out.println("Your total discout is: " + totalDiscount);
+		System.out.println("You have to pay : " + amountTopay + "\n");
+		System.out.println("  " +  "\n");
+	} 
 }
