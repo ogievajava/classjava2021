a8713647529cb227e485864d90ea9b217818b8b1
admin CIFO <admin CIFO@DESKTOP-340U509>
2021-04-26 11:47:13 +0200

    word string

---
+++ work4/src/work4/Operations.java
@@ -0,0 +1,86 @@
+package work4;
+
+
+public class Operations {
+
+	public static void print(String wordtoPrint) {
+
+		System.out.print(wordtoPrint);
+	}
+
+	
+	public static void print(char chartoPrint) {
+
+		System.out.print(chartoPrint);
+	}
+
+	
+	public static void linebreak() {
+		System.out.print("\n");
+	}
+
+	
+	public static void printString(String wordtoPrint) {
+
+		int i = 0;
+		linebreak();
+		int sizeWordtoPrint = wordtoPrint.length();
+		
+		while (i < sizeWordtoPrint) {
+			print("loop number: " + (i + 1) + " - " );
+			
+			print("letter: " + wordtoPrint.charAt(i));
+			linebreak();
+
+			i++; // i = i +1;
+
+		}
+	}
+	
+		public static void printStringH(String wordtoPrint) {
+
+			int i = 0;
+		
+			int sizeOfWordsIntroduce = wordtoPrint.length();
+			while (i < sizeOfWordsIntroduce) {
+				print("loop number: " + (i + 1) + "  " );
+				
+				print(wordtoPrint.charAt(i));
+			
+
+				i++; // i = i +1;
+
+			}
+		}
+		
+		public static void printStringV(String wordtoPrint) {
+
+			int i = 0;
+			linebreak();
+			int sizeOfWordsIntroduce = wordtoPrint.length();
+			while (i < sizeOfWordsIntroduce) {
+				print("  " );
+				
+				print(wordtoPrint.charAt(i));
+			
+				i++; // i = i +1;
+
+			}
+	}
+		
+		public static void printStringGion(String wordtoPrint) {
+
+			int i = 0;
+			linebreak();
+			int sizeOfWordsIntroduce = wordtoPrint.length();
+			while (i < sizeOfWordsIntroduce) {
+				print((i + 1) + "  " );
+				
+				print(wordtoPrint.charAt(i));
+			
+				i++; // i = i +1;
+				linebreak();
+			}
+	}
+		
+}
---
+++ work4/src/work4/Test.java
@@ -0,0 +1,33 @@
+package work4;
+
+
+import java.util.Scanner;
+
+public class Test {
+
+		public static void main(String[] args) {
+
+			
+			Scanner reader = new Scanner (System.in);
+			System.out.println("Write some string ...");
+			String word = reader.nextLine();
+			
+			Operations.print(word);
+			System.out.println("\n");
+			Operations.linebreak();
+			Operations.printString(word);
+			System.out.println("\n");
+			Operations.printStringH(word);
+			System.out.println("\n");
+			Operations.printStringV(word);
+			System.out.println("\n");
+			Operations.printStringGion(word);
+			System.out.println("\n");
+			System.out.println("Bye bye my friend...");
+			
+			
+			
+		}
+
+	}
+
